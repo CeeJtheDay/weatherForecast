e8031c1a1d089116dc02ea4f63f9734b156ecc41
CJ Conrad <carlj.conrad@gmail.com>
2019-10-26 13:27:58 -0700

    button appear, still working on local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,119 @@
 // object for storing previous cities searched
-var citiesSearched = {};
+// var citiesSearched = {};
 // creates a new div
-var newDiv = $("<div>");
 // -------USER FLOW---------
 // user arrives at page
 //  Check local storage for stored object
 // user enters a city ro get ciurrent weather
 //      i. capture the value of input box
-var cityName = $("#search").val();
 //      ii. add it to the query URL
-
 //  1. ajax call
 //      i. build query URL
-var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&APPID=" + APIkey;
-var forecastUrl;
 //      11. build api key variable
-var APIkey = "744a700e87c190e4236cfc5a94cd8aff";
 //      111. response funtion
-function getCityWeather() {
-$.ajax ({
-    url: currentWeatherURL,
-    method: "GET"
-}).then (function(response) {
-    console.log(response);
-    var curTemp = response.main.temp;
-    console.log(curTemp);
-    var curHumid = response.main.humidity;
-    console.log(curHumid);
-    var curWind = response.wind.speed;
-    console.log(curWind);
-    var uvIndex;
+$(document).ready(function () {
+    var searchHist = [];
+    var APIkey = "166a433c57516f51dfab1f7edaed8413";
+    // loadSearchHist();
+    $("#submit").on("click", function () {
+        event.preventDefault();
+        var cityName = $("#search").val();
+        searchHist.push(cityName);
+        createNewCityBtn(cityName);
+        getCurrentWeather(cityName);
+    });
 
-    var cityID = response.id
-    console.log(cityID);
-    forecastURL =  "http://api.openweathermap.org/data/2.5/forecast?id=" + cityID +"&APPID=" + APIkey;
-    $.ajax ({
-        url: forecastURL,
-        method: "GET"
-    }).then
+
+    function getCurrentWeather(city) {
+        console.log($("#search").val());
+        var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + APIkey;
+        //  2. Grab current city from API
+        $.ajax({
+            url: currentWeatherURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            //      i. temperature
+            var curTemp = response.main.temp;
+            console.log(curTemp);
+            var tempDiv = $("<div id='cityTemp'>");
+            tempDiv.text("Current temperature = " + curTemp);
+            $("#currentWeather").append(tempDiv);
+            //      11. Humidity
+            var curHumid = response.main.humidity;
+            console.log(curHumid);
+            var humidDiv = $("<div id='cityHumid'>");
+            humidDiv.text("Humidity = " + curHumid);
+            $("#currentWeather").append(humidDiv);
+            //      iii. Wind Speed
+            var curWind = response.wind.speed;
+            console.log(curWind);
+            var windDiv = $("<div id='cityWind'>");
+            windDiv.text("Wind Speed = " + curWind);
+            $("#currentWeather").append(windDiv);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            getUvIndex(lat, lon);
+            var cityID = response.id;
+            console.log(cityID);
+            //      iiii. UV Index
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            getUvIndex(lat, lon);
+            //      iiiii. 5 day forecast
+            fiveDayForecast(cityID)
+            
+        });
+
+    };
+
+    function fiveDayForecast(ID) {
+        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?id=" + ID + "&units=imperial&cnt=5&APPID=" + APIkey;
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function (fiveDay) {
+            console.log(fiveDay);
+
+        });
+
+    };
+
+    function getUvIndex(lat, lon) {
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + APIkey + "&lat=" + lat + "&lon=" + lon + "&cnt=1";
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function (uvInd){
+            console.log(uvInd);
+        })
+    }
+
+    // function loadSearchHist() {
+    //     searchHist = localStorage.getItem(JSON.parse("CJ's Weather Board"));
+    //     if (localStorage.getItem(JSON.parse("CJ's Weather Board")) === null) {
+    //         localStorage.setItem("CJ's Weather Board", JSON.stringify(searchHist));
+    //     } 
+    //     for (var i = 0; i < searchHist.length; i++) {
+    //         var newBtn = $("<li class='cityBtn list-group-item>").text(searchHist[i]);
+    //         $(".list-group").append(newBtn);
+    //     }
+    // }
+
+    function createNewCityBtn(cityName) {
+        var newBtn = $("<button class='cityBtn'>");
+        newBtn.text(cityName);
+        $("#searchedCities").append(newBtn);
+    };
+
     
-    })
-};
+});
 
-// current weather is displayed to right
-//  2. Grab current city from API function showCurretCity ()
-//      show city namew and todays date
-//      i. temperature
-//      11. Humidity
-//      iii. Wind Speed
+    
+
+//      current weather is displayed to right
+//      show city name and todays date
 //      iiii. UV index
-
 // 5 day forecast is diplayed below current weather
 //      i. 5 days starting with today
 //      ii. temperature
